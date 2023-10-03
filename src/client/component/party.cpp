f27d693c5a03a22cc6c2b06e470ac685f6243328
dontknowletsplay <77815199+whydoesanyonecare@users.noreply.github.com>
2023-10-03 15:54:47 +0300

    add workshop downloader

--- src/client/component/party.cpp
+++ src/client/component/party.cpp
@@ -14,6 +14,8 @@
 #include <utils/info_string.hpp>
 #include <utils/cryptography.hpp>
 #include <utils/concurrency.hpp>
+#include "steamcmd.hpp"
+#include <thread>
 
 namespace party
 {
@@ -191,7 +193,10 @@ namespace party
 
 			const auto mod_id = info.get("modId");
 			const auto FastDL = info.get("sv_wwwBaseURL"); //Check fastDL dvar for url
+			const auto workshop_id = info.get("workshop_id"); //Check workshop_id dvar for id
 
+			printf("FastDL: %s\n", FastDL.c_str());
+			printf("Workshop id: %s\n", workshop_id.c_str());
 
 			//const auto hostname = info.get("sv_hostname");
 			const auto playmode = info.get("playmode");
@@ -202,8 +207,8 @@ namespace party
 			{
 				const auto usermap_id = workshop::get_usermap_publisher_id(mapname);
 
-				if (workshop::check_valid_usermap_id(mapname, usermap_id, FastDL) &&
-					workshop::check_valid_mod_id(mod_id, FastDL))
+				if (workshop::check_valid_usermap_id(mapname, usermap_id, workshop_id) &&
+					workshop::check_valid_mod_id(mod_id, workshop_id))
 				{
 					if (is_connecting_to_dedi)
 					{
@@ -213,6 +218,7 @@ namespace party
 					//connect_to_session(target, hostname, xuid, mode);
 					connect_to_lobby_with_mode(target, mode, mapname, gametype, usermap_id, mod_id);
 				}
+
 			}, scheduler::main);
 		}
 
