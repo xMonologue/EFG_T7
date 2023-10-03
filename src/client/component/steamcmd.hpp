e1ec66dd7c90a939ce526944943740f1605c0126
dontknowletsplay <77815199+whydoesanyonecare@users.noreply.github.com>
2023-10-03 16:00:26 +0300

    Create steamcmd.hpp

---
+++ src/client/component/steamcmd.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#ifndef STEAMCMD_HPP
+#define STEAMCMD_HPP
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <thread>
+#include <stdio.h>
+#include <filesystem>
+#include <curl/curl.h>
+#include <zlib.h>
+#include <zip.h>
+#include "unzip.h"
+#include <utils/io.hpp>
+#include "steamcmd.hpp"
+#include "std_include.hpp"
+#include "command.hpp"
+#include "component/steam_proxy.hpp"
+#include <stdlib.h>
+#include "component/party.hpp"
+#include "loader/component_loader.hpp"
+#include "game/game.hpp"
+#include "steam/steam.hpp"
+#include "network.hpp"
+#include "workshop.hpp"
+#include <utils/hook.hpp>
+#include <utils/string.hpp>
+#include <utils/info_string.hpp>
+#include <version.hpp>
+#include "game/utils.hpp"
+#include <fstream>
+#include <direct.h>
+#include <limits.h>
+#include <chrono>
+#include "party.hpp"
+
+namespace steamcmd
+{
+	int start_new_process(const char* exePath, bool Hide_Window, bool waittill_done, const char* arguments);
+	int extract_steamcmd();
+	size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream);
+	int setup_steamcmd();
+	void moveFolder(const std::string& sourceFolderPath, const std::string& destinationFolderPath);
+	int Download_Workshop_Item(std::string workshop_id, std::string modtype);
+}
+
+#endif // STEAMCMD_HPP
