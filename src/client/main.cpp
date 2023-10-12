261f28299e164938ed7937d5fecc8848dc0bac44
dontknowletsplay <77815199+whydoesanyonecare@users.noreply.github.com>
2023-10-12 13:59:05 +0300

    Update main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -281,7 +281,7 @@ int main()
 
 			if (!utils::io::file_exists(launcher::get_launcher_ui_file().generic_wstring()))
 			{
-				throw std::runtime_error("T7 EFG needs an active internet connection for the first time you launch it.\nYou can manually copy t7efg folder to localappdata to fix this error.");
+				throw std::runtime_error("T7efg files could not be found. \nCopy T7efg folder to localappdata to fix this error.");
 			}
 
 			const auto client_binary = "BlackOps3.exe"s;
